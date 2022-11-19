f3fae1793b1e30165892208cc316cbcb1acf4b7e
ML <70348681+mxl1n@users.noreply.github.com>
2022-11-18 17:08:19 -0800

    add learning rate var

--- contextualbandits/online.py
+++ contextualbandits/online.py
@@ -3756,6 +3756,7 @@ class NeuralBandit(_BasePolicyWithExploit):
                                            [n.parameters() for n in self.networks]),
                                     self.learning_rate)
         self.loss = torch.nn.MSELoss()
+        self.learning_rate = learning_rate
 
 
     def _add_common_lin(self, alpha, lambda_, fit_intercept, use_float, method, nchoices, njobs):
